1235d69ec16085694bec4618d0c7de982294172a
zhangcn <sunshine361163@163.com>
2021-07-03 17:18:13 +0800

    项目基础环境v1.0

---
+++ src/main/java/com/baizhi/GitTest2021Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTest2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTest2021Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/baizhi/GitTest2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTest2021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
