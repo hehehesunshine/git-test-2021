dbec2b8f7857b4ddd721c19274448855f311115f
zhangcn <sunshine361163@163.com>
2021-07-03 17:38:56 +0800

    添加很多功能

---
+++ src/test/java/com/baizhi/GitTestce2.java
@@ -0,0 +1,19 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestce2 {
+
+    @Test
+    void contextLoads() {
+        int a=10;
+        int b=20;
+    }
+    public static void main(String[] args){
+        System.out.println("-----测试2------");
+        int a=30;
+        int b=40;
+    }
+}
---
+++ src/test/java/com/baizhi/GitTestce3.java
@@ -0,0 +1,17 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestce3 {
+
+    @Test
+    void contextLoads() {
+        int a=10;
+        int b=20;
+    }
+    public static void main(String[] args){
+        System.out.println("-----测试3------");
+    }
+}
