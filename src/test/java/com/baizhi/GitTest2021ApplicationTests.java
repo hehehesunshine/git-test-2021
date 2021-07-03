613b0bc65e64da7eddb9b607287c1293b8c269b1
zhangcn <sunshine361163@163.com>
2021-07-03 17:33:30 +0800

    idea测试添加一个主函数

--- src/test/java/com/baizhi/GitTest2021ApplicationTests.java
+++ src/test/java/com/baizhi/GitTest2021ApplicationTests.java
@@ -11,5 +11,7 @@ class GitTest2021ApplicationTests {
         int a=10;
         int b=20;
     }
-
+    public static void main(String[] args){
+        System.out.println("-----测试------");
+    }
 }
